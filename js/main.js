b31b932f2d673941373da678e3b58d6276251c56
michele <michele.giovagnoli1@gmail.com>
2023-02-21 15:50:11 +0100

    Aggiungo i commenti al file main.js

--- js/main.js
+++ js/main.js
@@ -9,13 +9,17 @@
 const nome = prompt('inserisci il tuo nome');
 document.getElementById('nome').innerHTML = `Il tuo nome e: ${nome}`;
 
+//quando l'utente ha inserito il nome, chiedo il cognome e lo stampo
 const cognome = prompt('inserisci il tuo cognome');
 document.getElementById('cognome').innerHTML = `Il tuo cognome e: ${cognome}`;
 
+//stesso procedimento per il colore
 const colore = prompt('inserisci il tuo colore preferito');
 document.getElementById('colore').innerHTML = `il tuo colore preferito e: ${colore}`;
 
+//creo una variabile che contiene la stringa 23 da aggiungere alla creazione della password
 let numero = ('23');
 
+//sommo le varibaili e creo la password 
 let password = nome + cognome + colore + numero;
 document.getElementById('password').innerHTML = `La password generata e: ${password}`;
